ceffb8b23a3ca4c44fa2bd494fa67e790c2dc07c
nk1223 <nk1223@ic.ac.uk>
2024-10-17 07:08:42 +0000

    classifiers.py cleaned + commented

--- src/classifiers.py
+++ src/classifiers.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPClassifier
+from sklearn.base import ClassifierMixin
 
 C_LOGISTIC_REGRESSION = [1e-6, 1e-5, 1e-4, 0.001, 0.01, 0.1, 1, 10]
 RF_PARAMETERS = {'n_estimators': [100, 200, 20, 50, 100, 200, 500],
@@ -14,34 +15,47 @@ RF_PARAMETERS = {'n_estimators': [100, 200, 20, 50, 100, 200, 500],
 MLP_PARAMETERS = {'hidden_layer_sizes':[(100,), (200,), (100, 100), (200, 200)],
              'alpha': [1, 10, 100]}
 
-def fit_classifier(X_train: pd.DataFrame, y_train: pd.DataFrame, model: str, cv=False):
-    
-    if model=='logistic_regression':
-        clf=train_LogisticRegression(X_train, y_train, cv)
-        
-    elif model=='random_forest':
-        clf = train_RandomForest(X_train, y_train, cv)
-    elif model == 'mlp':
-        clf = train_MLP(X_train, y_train, cv)
 
-    return clf
+def drop_zero_cols(X_train: pd.DataFrame, X_test: pd.DataFrame = None) -> tuple:
+    """
+    Drops columns from the input DataFrames where all column values are zero.
 
+    Parameters:
+    X_train: pd.DataFrame
+        The training data
+    X_test: pd.DataFrame, optional
+        The test data
 
-def drop_zero_cols(X_train, X_test = None, X_test_specific = None):
+    Returns:
+    If X_test is not provided, returns the modified X_train with zero-sum columns dropped.
+    If X_test is provided, returns both X_train and X_test with zero-sum columns dropped.
+    """
+
     all_summed = X_train.sum()
     cols_to_drop = [col for col in X_train.columns if all_summed[col] == 0]
     X_train = X_train.drop(cols_to_drop, axis=1)
     if X_test is None:
-        return X_train
+        return X_train, None
     X_test = X_test.drop(cols_to_drop, axis=1)
-    if X_test_specific is not None:
-        X_test_specific = X_test_specific.drop(cols_to_drop, axis=1)
-        #return X_train, X_test, X_test_specific
-        return X_train, X_test, X_test_specific   
     return X_train, X_test
 
 
-def scale_features(X_train, X_test=None, X_test_specific = None):
+def scale_features(X_train: pd.DataFrame, X_test: pd.DataFrame=None) -> tuple:
+    """
+    Scales the features in X_train by standardizing them.
+    If X_test is provided, it scales the test set using the same mean and standard deviation as X_train.
+
+    Parameters:
+    X_train: pd.DataFrame
+        The training data
+    X_test: pd.DataFrame, optional
+        The test data (default is None)
+
+    Returns:
+    If X_test is not provided, returns the standardized X_train.
+    If X_test is provided, returns both the standardized X_train and X_test.
+    """
+
     X_train_values = X_train.values
     all_means = X_train_values.mean(axis=0)
     all_stds = X_train_values.std(axis=0)
@@ -49,26 +63,38 @@ def scale_features(X_train, X_test=None, X_test_specific = None):
 
     X_train = pd.DataFrame((X_train_values - all_means) / all_stds, columns=X_train.columns)
     if X_test is None:
-        return X_train
+        return X_train, None
     X_test = pd.DataFrame((X_test.values - all_means) / all_stds, columns=X_train.columns)
-    if X_test_specific is not None:
-        X_test_specific = pd.DataFrame((X_test_specific.values - all_means) / all_stds, columns=X_train.columns)
-        return X_train, X_test, X_test_specific
     return X_train, X_test
 
-# def select_features(X_train: pd.DataFrame, y_train: pd.DataFrame, n_features: int = 20000) -> list:
-#     '''
-#      do RFE to select the top X features
-#     '''
-#     estimator = LogisticRegression()
-#     selector = RFE(estimator, n_features_to_select=n_features, step=0.05)
-#     selector = selector.fit(X_train, y_train)
-#     selected_feats = selector.get_feature_names_out()
-
-#     return selected_feats
-
-def validate_clf(clf, X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame,
+def validate_clf(clf: ClassifierMixin, X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame,
                  y_test: pd.DataFrame) -> tuple:
+    """
+    Evaluates the classifier's performance on both the training and test datasets.
+    Prints the accuracy and AUC (Area Under the Curve) for both training and test sets.
+
+    Parameters:
+    -----------
+    clf: ClassifierMixin
+        The classifier to be evaluated (any estimator with `predict` and `predict_proba` methods).
+    X_train: pd.DataFrame
+        The feature data for the training set.
+    y_train: pd.DataFrame
+        The target labels for the training set.
+    X_test: pd.DataFrame
+        The feature data for the test set.
+    y_test: pd.DataFrame
+        pd.DataFrameThe target labels for the test set.
+
+    Returns:
+    --------
+    tuple: A tuple containing:
+      - Training accuracy
+      - Training AUC
+      - Test accuracy
+      - Test AUC (if computable)
+    """
+
     y_train_pred = clf.predict(X_train)
     train_acc = accuracy_score(y_train, y_train_pred)
     print('Training accuracy: ', train_acc)
@@ -82,11 +108,30 @@ def validate_clf(clf, X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.D
         test_auc = roc_auc_score(y_test, clf.predict_proba(X_test)[:, 1])
         print('Test auc: ', test_auc)
     except:
-        print('Auc impossible, do you have only one classe?')
+        print('Auc impossible, do you have only one class?')
         return train_acc, test_acc
     return train_acc, train_auc, test_acc, test_auc
 
 def train_LogisticRegression(X_train: pd.DataFrame, y_train: pd.DataFrame, cv: bool = False) -> LogisticRegression:
+    """
+    Trains a logistic regression model using either a fixed regularization parameter or cross-validation.
+
+    Parameters:
+    ------------
+    X_train: pd.DataFrame
+        The feature data for the training set.
+    y_train: pd.DataFrame
+        The target labels for the training set.
+    cv: bool, optional
+        If True, performs cross-validation to find the best regularization parameter. 
+        If False, trains the model using a fixed regularization parameter (default is False).
+
+    Returns:
+    ---------
+    LogisticRegression: A trained logistic regression model.
+
+    If `cv=True`, the function also prints the best regularization parameter (`C`) found during cross-validation.
+    """
     if not cv:
         clf = LogisticRegression(C = 0.001)
         clf.fit(X_train, y_train)
@@ -98,6 +143,25 @@ def train_LogisticRegression(X_train: pd.DataFrame, y_train: pd.DataFrame, cv: b
     return clf
 
 def train_RandomForest(X_train: pd.DataFrame, y_train: pd.DataFrame, cv: bool = False) -> RandomForestClassifier:
+    """
+    Trains a random forest classifier using either fixed hyperparameters or cross-validation for hyperparameter tuning.
+
+    Parameters:
+    -----------
+    X_train: pd.DataFrame
+        The feature data for the training set.
+    y_train: pd.DataFrame
+        The target labels for the training set.
+    cv: bool, optional
+        If True, performs cross-validation to find the best hyperparameters. 
+                         If False, trains the model using fixed hyperparameters (default is False).
+
+    Returns:
+    --------
+    RandomForestClassifier: A trained random forest classifier.
+
+    If `cv=True`, the function performs a grid search to tune the hyperparameters and prints the best parameters found.
+    """
     if not cv:
         clf = RandomForestClassifier(max_depth=10, n_estimators=100)
         clf.fit(X_train, y_train)
@@ -112,6 +176,26 @@ def train_RandomForest(X_train: pd.DataFrame, y_train: pd.DataFrame, cv: bool = 
     return clf
 
 def train_MLP(X_train: pd.DataFrame, y_train: pd.DataFrame, cv: bool = False) -> MLPClassifier:
+    """
+    Trains a Multi-layer Perceptron (MLP) classifier using either fixed hyperparameters or cross-validation for hyperparameter tuning.
+
+    Parameters:
+    -----------
+    X_train: pd.DataFrame
+        The feature data for the training set.
+    y_train: pd.DataFrame
+        The target labels for the training set.
+    cv: bool, optional
+        If True, performs cross-validation to find the best hyperparameters. 
+        If False, trains the model using fixed hyperparameters (default is False).
+
+    Returns:
+    --------
+    MLPClassifier: A trained MLP classifier.
+
+    If `cv=True`, the function performs a grid search to tune the hyperparameters and prints the best parameters found.
+    """
+
     if not cv:
         clf = MLPClassifier(hidden_layer_sizes=(100, 100), alpha=0.01)
         clf.fit(X_train, y_train)
@@ -125,26 +209,64 @@ def train_MLP(X_train: pd.DataFrame, y_train: pd.DataFrame, cv: bool = False) ->
 
     return clf
 
-def fit_classifiers(X_train: pd.DataFrame, y_train: pd.DataFrame, models: list, cv=False):
-    trained_models = []
+def fit_classifier(X_train: pd.DataFrame, y_train: pd.DataFrame, model: str, cv=False):
+    """
+    Trains a classifier based on the specified model type using the provided training data.
+
+    Parameters:
+    -----------
+    X_train: pd.DataFrame
+        The feature data for the training set.
+    y_train: pd.DataFrame
+        The target labels for the training set.
+    model: str
+        The type of classifier to train. Supported values are: 'logistic_regression', 'random_forest', 'mlp'.
+    cv: bool, optional)
+        If True, performs cross-validation during model training to tune hyperparameters.
+        If False, trains the model using fixed hyperparameters (default is False).
+
+    Returns:
+    --------
+    object: A trained classifier object based on the specified model.
+    """
     
-    for model in models:
-        print(f'Model: {model}')
-        if model=='logistic_regression':
-            clf=train_LogisticRegression(X_train, y_train, cv)
-            
-        elif model=='random_forest':
-            clf = train_RandomForest(X_train, y_train, cv)
-        elif model == 'mlp':
-            clf = train_MLP(X_train, y_train, cv)
-            
-        trained_models.append(clf)
+    if model=='logistic_regression':
+        clf=train_LogisticRegression(X_train, y_train, cv)
         
-        return trained_models
-            
+    elif model=='random_forest':
+        clf = train_RandomForest(X_train, y_train, cv)
+    elif model == 'mlp':
+        clf = train_MLP(X_train, y_train, cv)
+
+    return clf            
 
 def fit_validate_classifiers(X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame,
                              y_test: pd.DataFrame, models: list, cv: bool = False) -> tuple:
+    """
+    Trains and validates multiple classifiers on the provided training and test sets.
+
+    Parameters:
+    ------------
+    X_train: pd.DataFrame
+        The feature data for the training set.
+    y_train: pd.DataFrame
+        The target labels for the training set.
+    X_test: pd.DataFrame
+        The feature data for the test set.
+    y_test: pd.DataFrame
+        The target labels for the test set.
+    models: list
+        A list of model names (as strings) to be trained and validated. Supported models are: 'logistic_regression', 'random_forest', 'mlp'.
+    cv: bool, optional
+        If True, performs cross-validation during model training to tune hyperparameters.
+        If False, trains the model using fixed hyperparameters (default is False).
+
+    Returns:
+    ---------
+    tuple: A tuple containing:
+      - A list of trained models.
+      - A list of the results (training and test accuracy, AUC, etc.) for each model.
+    """
     trained_models, all_results = [], []
     for model in models:
         print('Model: ', model)
@@ -164,30 +286,3 @@ def fit_validate_classifiers(X_train: pd.DataFrame, y_train: pd.DataFrame, X_tes
         all_results.append(results)
 
     return trained_models, all_results
-
-
-# def fit_predict_classifiers_converge(X_train: pd.DataFrame, y_train: pd.DataFrame, models: list, cv: bool = False):
-#     trained_models = []
-    
-#     train_preds = []
-#     test_preds = []
-    
-#     for model in models:
-#         print(f'Model: {model}')
-#         if model =='logistic_regression':
-#             clf = train_LogisticRegression(X_train, y_train, cv)
-            
-#             trained_models.append(clf)
-            
-#         elif model == 'random_forest':
-#             clf = train_RandomForest(X_train, y_train, cv)
-            
-#             trained_models.append(clf)
-            
-           
-#         elif model == 'mlp':
-#             clf = train_MLP(X_train, y_train, cv)
-            
-#             trained_models.append(clf)
-    
-#     return trained_models
